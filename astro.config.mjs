9c18129bb8135f5775abb74d3cc80fea3c93f667
jieer <1458525036@qq.com>
2026-09-18 09:51:35 +0800

    update: latest changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
-import keystatic from '@keystatic/astro';
 
-export default defineConfig({
-  output: 'static',
-  adapter: cloudflare(),
-  integrations: [keystatic(), react()],
-});
+// https://astro.build/config
+export default defineConfig({});
